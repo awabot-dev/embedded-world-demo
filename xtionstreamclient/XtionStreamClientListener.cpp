5e5f7ecbc52b97017460d9993bb47baf2537c34d
One <one@awabot.com>
2015-12-22 11:47:02 +0100

    Comments removed

--- xtionstreamclient/XtionStreamClientListener.cpp
+++ xtionstreamclient/XtionStreamClientListener.cpp
@@ -8,18 +8,5 @@ void XtionStreamClientListener::onFrame(Frame* frame)
 {
 	std::cout << "New frame !" << std::endl;
 
-	//TODO
-
-	/*
-	static int frameCount = 0;
-
-	std::ostringstream fileName;
-	fileName << "/tmp/frame" << (++frameCount < 10 ? "0" : "") << frameCount << ".rgb24";
-
-	std::ofstream file(fileName.str().c_str());
-	file.write(frame->data, frame->size);
-	file.close();
-
-	std::cout << fileName.str() << " dumped" << std::endl;
-	*/
+	//TODO frame processing
 }
